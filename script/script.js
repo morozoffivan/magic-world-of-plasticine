227a33bdf98e193bcef55da75ada517a9c099635
Morozov Ivan <morozoff.one@yandex.ru>
2024-10-29 15:59:14 +0300

    added header section

--- script/script.js
+++ script/script.js
@@ -1,23 +1,30 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    const btnNavToggler = document.querySelector('#navToggler'),
-          navbar = document.querySelector('#navbar');
+document.addEventListener("DOMContentLoaded", () => {
+  const btnNavToggler = document.querySelector("#navToggler"),
+        navbar = document.querySelector("#navbar");
 
-    function monitorChanger(width){
-        if(width < 1200) {
-            console.log(width);
-            navbar.classList.remove('flex-container-header');
-            navbar.classList.add('hidden');
-            btnNavToggler.classList.remove('hidden');
-            btnNavToggler.classList.add('navbar-toggler');
-        }
+  function monitorChanger(width) {
+    if (width < 1200) {
+      console.log(width);
+      navbar.classList.remove("flex-container-header");
+      navbar.classList.add("hidden");
+      btnNavToggler.classList.remove("hidden");
+      btnNavToggler.classList.add("navbar-toggler");
     }
-    
-    
-    monitorChanger(window.innerWidth);
-    
-    
-    btnNavToggler.addEventListener('click', ()=> {
-        navbar.classList.remove('hidden')
-        navbar.classList.add('show-nav');
-    })
-})
+  }
+
+  function onOpenNav(btnToggler, bar){
+    btnToggler.addEventListener("click", () => {
+        if (bar.className === "hidden") {
+          bar.classList.remove("hidden");
+          bar.classList.add("show-nav");
+        } else {
+            bar.classList.add("hidden");
+            bar.classList.remove("show-nav");
+        }
+      });
+  }
+
+  monitorChanger(window.innerWidth);
+  onOpenNav(btnNavToggler, navbar);
+  
+});
